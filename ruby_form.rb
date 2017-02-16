33a47295cf848444ba604b0ff0936196b367a469
Patrick <pkeenan516@gmail.com>
2017-02-15 21:41:46 -0500

    first commit

---
+++ ruby_form.rb
@@ -0,0 +1,10 @@
+# This is the ruby code I will be writing to put some kind of database on my about_me website.
+
+# Get the name of user
+  class User
+    f_name = @f_name.New()
+    l_name = @l_name.New()
+  end
+# Get the email address of user
+
+# Get comment from user.
